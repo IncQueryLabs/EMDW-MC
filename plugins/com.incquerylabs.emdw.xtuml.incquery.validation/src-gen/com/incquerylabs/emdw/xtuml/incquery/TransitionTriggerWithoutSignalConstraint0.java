2a20e0272acfed91c5c14705fde8618b789e348c
Abel Hegedus <abel.hegedus@incquerylabs.com>
2015-06-25 17:46:28 +0200

    #105 added generated code for validation

---
+++ plugins/com.incquerylabs.emdw.xtuml.incquery.validation/src-gen/com/incquerylabs/emdw/xtuml/incquery/TransitionTriggerWithoutSignalConstraint0.java
@@ -0,0 +1,81 @@
+package com.incquerylabs.emdw.xtuml.incquery;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import org.eclipse.incquery.validation.core.api.Severity;
+import org.eclipse.incquery.validation.core.api.IConstraintSpecification;
+import org.eclipse.incquery.runtime.api.IPatternMatch;
+import org.eclipse.incquery.runtime.api.IQuerySpecification;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
+import org.eclipse.incquery.runtime.exception.IncQueryException;
+
+import com.incquerylabs.emdw.xtuml.incquery.util.TransitionTriggerWithoutSignalQuerySpecification;
+
+public class TransitionTriggerWithoutSignalConstraint0 implements IConstraintSpecification {
+
+    private TransitionTriggerWithoutSignalQuerySpecification querySpecification;
+
+    public TransitionTriggerWithoutSignalConstraint0() throws IncQueryException {
+        querySpecification = TransitionTriggerWithoutSignalQuerySpecification.instance();
+    }
+
+    @Override
+    public String getMessageFormat() {
+        return "Transition $transition$ has a trigger $trigger$ without signal";
+    }
+
+
+    @Override
+    public Map<String,Object> getKeyObjects(IPatternMatch signature) {
+        Map<String,Object> map = ImmutableMap.of(
+            "trigger",signature.get("trigger")
+        );
+        return map;
+    }
+
+    @Override
+    public List<String> getKeyNames() {
+        List<String> keyNames = ImmutableList.of(
+            "trigger"
+        );
+        return keyNames;
+    }
+
+    @Override
+    public List<String> getPropertyNames() {
+        List<String> propertyNames = ImmutableList.of(
+            "transition"
+        );
+        return propertyNames;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricPropertyNames() {
+        Set<List<String>> symmetricPropertyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricPropertyNamesSet;
+    }
+
+    @Override
+    public Set<List<String>> getSymmetricKeyNames() {
+        Set<List<String>> symmetricKeyNamesSet = ImmutableSet.<List<String>>of(
+        );
+        return symmetricKeyNamesSet;
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.ERROR;
+    }
+
+    @Override
+    public IQuerySpecification<? extends IncQueryMatcher<? extends IPatternMatch>> getQuerySpecification() {
+        return querySpecification;
+    }
+
+}

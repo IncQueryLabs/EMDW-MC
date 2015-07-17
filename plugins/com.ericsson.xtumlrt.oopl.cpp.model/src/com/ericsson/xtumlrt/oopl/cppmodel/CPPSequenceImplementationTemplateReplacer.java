1701b41b18bab328b4d3713c89ee18fa1cb57e00
Tamás Borbás <borbastomi@gmail.com>
2015-07-17 13:34:31 +0200

    Modification in CPPSequenceImplementation and its template replacer
    	correct and create body for CPPSequenceImplementation's operations
    	correct CPPSequenceImplementationTemplateReplacer methods

--- plugins/com.ericsson.xtumlrt.oopl.cpp.model/src/com/ericsson/xtumlrt/oopl/cppmodel/CPPSequenceImplementationTemplateReplacer.java
+++ plugins/com.ericsson.xtumlrt.oopl.cpp.model/src/com/ericsson/xtumlrt/oopl/cppmodel/CPPSequenceImplementationTemplateReplacer.java
@@ -11,44 +11,51 @@ public class CPPSequenceImplementationTemplateReplacer extends BaseContainerImpl
      * 
      **************************************************/
     
-    public static String generateElementAtIndex(CPPSequenceImplementation impl, String collection, String index) {
-        String template = impl.getElementAtIndexTemplate();
+    public static String generateAny(CPPSequenceImplementation impl, String collection, String valueType, String result) {
+        String template = impl.getAnyTemplate();
         Builder<String, String> map = ImmutableMap.builder();
         map.put("$collection$", collection);
-        map.put("$index$", index);
-        return generateTemplate(template, map.build());
-    }
-    
-    public static String generateInsertElementAtIndex(CPPSequenceImplementation impl, String collection, String value, String index) {
-        String template = impl.getInsertElementAtIndexTemplate();
-        Builder<String, String> map = ImmutableMap.builder();
-        map.put("$collection$", collection);
-        map.put("$value$", value);
-        map.put("$index$", index);
-        return generateTemplate(template, map.build());
-    }
-    
-    public static String generateReplaceElementAtIndex(CPPSequenceImplementation impl, String collection, String value, String index) {
-        String template = impl.getReplaceElementAtIndexTemplate();
-        Builder<String, String> map = ImmutableMap.builder();
-        map.put("$collection$", collection);
-        map.put("$value$", value);
-        map.put("$index$", index);
+        map.put("$valueType$", valueType);
+        map.put("$result$", result);
         return generateTemplate(template, map.build());
     }
 
-    public static String generateCountOf(CPPSequenceImplementation impl, String collection, String value) {
+    public static String generateCountOf(CPPSequenceImplementation impl, String collection, String value, String result) {
         String template = impl.getCountOfTemplate();
         Builder<String, String> map = ImmutableMap.builder();
         map.put("$collection$", collection);
         map.put("$value$", value);
+        map.put("$result$", result);
         return generateTemplate(template, map.build());
     }
     
-    public static String generateAny(CPPSequenceImplementation impl, String collection) {
-        String template = impl.getAnyTemplate();
+    public static String generateElementAtIndex(CPPSequenceImplementation impl, String collection, String valueType, String index, String result) {
+        String template = impl.getElementAtIndexTemplate();
         Builder<String, String> map = ImmutableMap.builder();
         map.put("$collection$", collection);
+        map.put("$valueType$", valueType);
+        map.put("$index$", index);
+        map.put("$result$", result);
+        return generateTemplate(template, map.build());
+    }
+    
+    public static String generateInsertElementAtIndex(CPPSequenceImplementation impl, String collection, String value, String valueType, String index) {
+        String template = impl.getInsertElementAtIndexTemplate();
+        Builder<String, String> map = ImmutableMap.builder();
+        map.put("$collection$", collection);
+        map.put("$value$", value);
+        map.put("$valueType$", valueType);
+        map.put("$index$", index);
+        return generateTemplate(template, map.build());
+    }
+    
+    public static String generateReplaceElementAtIndex(CPPSequenceImplementation impl, String collection, String value, String valueType, String index) {
+        String template = impl.getReplaceElementAtIndexTemplate();
+        Builder<String, String> map = ImmutableMap.builder();
+        map.put("$collection$", collection);
+        map.put("$value$", value);
+        map.put("$valueType$", valueType);
+        map.put("$index$", index);
         return generateTemplate(template, map.build());
     }
 }

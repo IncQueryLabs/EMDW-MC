e04c49378d637a199009d3920e886b09f2b2e68c
tamasborbas <tamas.borbas@incquerylabs.com>
2016-03-29 09:56:23 +0200

    Add copyright to xtend, xtext, eiq and mwe2 files

--- plugins/com.incquerylabs.emdw.xtuml.incquery.validation/src-gen/com/incquerylabs/emdw/xtuml/incquery/FileNameCollisionConstraint0.java
+++ plugins/com.incquerylabs.emdw.xtuml.incquery.validation/src-gen/com/incquerylabs/emdw/xtuml/incquery/FileNameCollisionConstraint0.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2015-2016 IncQuery Labs Ltd. and Ericsson AB
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Akos Horvath, Abel Hegedus, Zoltan Ujhelyi, Daniel Segesdi, Tamas Borbas, Peter Lunk - initial API and implementation
- *******************************************************************************/
 package com.incquerylabs.emdw.xtuml.incquery;
 
 import java.util.List;
